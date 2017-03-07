2f2878bba987d103ec8460a1e76ccc2136948a5e
Juan Goovaerts <juangoovaerts@MacBook-Pro-de-Juan.local>
2017-03-07 00:40:04 -0500

    Initial Commit

---
+++ testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Juan Goovaerts on 7/03/17.
+//  Copyright © 2017 Juan Goovaerts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
